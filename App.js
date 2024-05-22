d0fbf068aa68ff0906af307394f1ca453cca82ca
Janish-1 <janish.pancholi11@gmail.com>
2024-05-22 14:47:11 +0530

    added back handler

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview'; // Import WebView component
+import { StatusBar, BackHandler } from 'react-native'; // Import BackHandler
+import React, { useEffect, useRef, useState } from 'react'; // Import useEffect, useRef, and useState
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [canGoBack, setCanGoBack] = useState(false); // State variable to track if WebView can go back
+  const webViewRef = useRef(null); // Reference to WebView component
+
+  // Function to handle the back button press
+  const handleBackButton = () => {
+    if (webViewRef.current && canGoBack) {
+      webViewRef.current.goBack();
+      return true; // Prevent default behavior
+    }
+    return false;
+  };
+
+  // Add event listener for hardware back button press when component mounts
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
+    // Remove event listener when component unmounts
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+    };
+  }, [canGoBack]); // Re-run effect when canGoBack changes
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'http://kutumbfront.ramo.co.in/' }} style={{ flex: 1 }} />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: 'http://kutumbfront.ramo.co.in/' }}
+        style={{ flex: 1 }}
+        // Update canGoBack state based on navigation state changes
+        onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,5 +43,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 30,
   },
 });
